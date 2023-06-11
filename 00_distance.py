882ae87bf4a140b465062cd7a27ca7e3be050e85
Kosinka <skosinka@yandex.ru>
2023-06-12 02:25:01 +0300

    00 resolved

--- 00_distance.py
+++ 00_distance.py
@@ -13,10 +13,19 @@ sites = {
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 distances = {}
 
-# TODO здесь заполнение словаря
-for city in sites:
-
-
+# sites_keys = sites.keys()
+for city in sites.keys():
+    coordinates = sites[city]
+    city_coordinate_x, city_coordinate_y = coordinates[0], coordinates[1]
+    for cities in sites.keys():
+        if city == cities:
+            pass
+        else:
+            second_city_coordinates = sites[cities]
+            second_city_coordinate_x, second_city_coordinate_y = second_city_coordinates[0], second_city_coordinates[1]
+            distance_key = city + ' ' + cities
+            distance = ((city_coordinate_x - second_city_coordinate_x) ** 2 + (city_coordinate_y - second_city_coordinate_y) ** 2) ** 0.5
+            distances[distance_key] = distance
 
 print(distances)
 
