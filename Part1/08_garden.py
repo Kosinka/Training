c20d8bf9778535eba80bbeb974830bb06c46cfe7
Kosinka <skosinka@yandex.ru>
2023-06-12 16:17:17 +0300

    08 resolved

--- Part1/08_garden.py
+++ Part1/08_garden.py
@@ -10,18 +10,30 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
-# TODO здесь ваш код
-
+garden_set = set(garden)
+meadow_set = set(meadow)
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
+print(garden_set, meadow_set)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
+both_flowers = set()
+for flower in garden_set:
+    if flower in meadow_set:
+        both_flowers.add(flower)
+print(both_flowers)
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+only_garden_flowers = set()
+for flower in garden_set:
+    if flower not in meadow_set:
+        only_garden_flowers.add(flower)
+print(only_garden_flowers)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+only_meadow_set = set()
+for flower in meadow_set:
+    if flower not in garden_set:
+        only_meadow_set.add(flower)
+print(only_meadow_set)
 
 
