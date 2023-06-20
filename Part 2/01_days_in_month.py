c65c5afbd067a840db4f7400a66a7eab8a4ef70f
Kosinka <skosinka@yandex.ru>
2023-06-21 01:23:41 +0300

    Part 2_01 resolved

--- Part 2/01_days_in_month.py
+++ Part 2/01_days_in_month.py
@@ -11,4 +11,27 @@ user_input = input("Введите, пожалуйста, номер месяц�
 month = int(user_input)
 print('Вы ввели', month)
 
-# TODO здесь ваш код
+if month == 1:
+    print(31)
+elif month == 2:
+    print(28)
+elif month == 3:
+    print(31)
+elif month == 4:
+    print(30)
+elif month == 5:
+    print(31)
+elif month == 6:
+    print(31)
+elif month == 7:
+    print(31)
+elif month == 8:
+    print(31)
+elif month == 9:
+    print(31)
+elif month == 10:
+    print(31)
+elif month == 11:
+    print(31)
+elif month == 12:
+    print(31)
